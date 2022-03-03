6fcd5a50b806679eabf57516077ceadf4ebbfb5e
Michaela Wilson <+>
2022-03-02 23:02:59 -0700

    commit tree 0edae87ebb7fcc45f563e0e8867251152ab57c89

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let result;
+  if (ride <= 400){
+    result = 'This one is on me!'
+    return result; 
+  } 
+  else if (ride > 2000 && ride <= 2500){
+    result = `I will gladly take your thirty bucks.`
+    return result;
+  }
+  else {
+    result = `No can do.`
+    return result;
+  }
+}
+  
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? `Ok, sounds good.`: `No go.`;
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip){
+    case 'generous':
+      return `Thank you so much.`;
+      break;
+    case 'not as generous':
+      return `Thank you.`;
+      break;
+    default:
+      return `Bye.`;
+      break;
+  }
 }
